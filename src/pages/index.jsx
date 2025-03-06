f54d71414116285bf33e9eec9520c6766b6c325f
Andre-Margarido <andre_margarido@hotmail.com>
2025-03-06 20:57:25 +0000

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <>
+      <main>
+        <nav>Menu aqui</nav>
+        <main>Conteudo aqui</main>
+        <footer>Footer aqui</footer>
+      </main>
+    </>
+  );
+}
